3a95338c583b29e51d71a4779c7264ed16de1cdd
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-18 16:55:37 -0600

    Updates

---
+++ precache-manifest.5bd8751615b1844e08a02db8416e5300.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66988122dcedca45d69b6c5b9237b551",
+    "url": "/filmbox/index.html"
+  },
+  {
+    "revision": "d39d9bde9da9960d9c58",
+    "url": "/filmbox/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b89da7bb80eb8362305b",
+    "url": "/filmbox/static/js/2.4d4d07bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/filmbox/static/js/2.4d4d07bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39d9bde9da9960d9c58",
+    "url": "/filmbox/static/js/main.10b353d1.chunk.js"
+  },
+  {
+    "revision": "ee95790270b7148123a5",
+    "url": "/filmbox/static/js/runtime-main.f3865698.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/filmbox/static/media/logo.5d5d9eef.svg"
+  }
+]);
