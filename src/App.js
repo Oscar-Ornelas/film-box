3c0f4ca1748005149c3fd5cf92ba8404deb9b704
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-26 22:15:35 -0600

    converted App component to function component with hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import FilmCard from './FilmCard'
 import FilmCardContainer from './FilmCardContainer'
 
-class App extends React.Component {
+function App() {
+  const [filmList, setFilmList] = useState([])
 
-  state = {
-    filmList: []
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=5b076c9b61e53ea3c493e7d25440c109&language=en-US&page=1")
     .then(response => response.json())
     .then(data => {
       data.results.forEach(film => {
         fetch(`https://api.themoviedb.org/3/movie/${film.id}?api_key=5b076c9b61e53ea3c493e7d25440c109&language=en-US`)
         .then(response => response.json())
-        .then(data => this.setState(prevState => ({filmList: [...prevState.filmList, data]})))
+        .then(data => setFilmList(prevFilmList => [...prevFilmList, data]))
       })
     })
-  }
+  }, []);
 
-  render(){
-    const films = this.state.filmList.map(film => {
-      return (
-        <FilmCard
-          title={film.title}
-          id={film.id}
-          overview={film.overview}
-          genres={film.genres}
-          posterUrl={`https://image.tmdb.org/t/p/w154${film.poster_path}`}
-        />
-      )
-    });
 
+  const films = filmList.map(film => {
     return (
-      <div>
-        <FilmCardContainer films={films}/>
-      </div>
-    );
-  }
+      <FilmCard
+        title={film.title}
+        id={film.id}
+        overview={film.overview}
+        genres={film.genres}
+        posterUrl={`https://image.tmdb.org/t/p/w154${film.poster_path}`}
+      />
+    )
+  });
+
+  return (
+    <div>
+      <FilmCardContainer films={films}/>
+    </div>
+  );
 }
 
 export default App;
--- src/FilmDetail.js
+++ src/FilmDetail.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FilmDetail() {
   return (
     <div>
-    
+      
     </div>
   )
 }
