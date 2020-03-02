52409dc64b6855ec48520313229a4a964e6bc03a
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-03-01 21:34:20 -0600

    Replaced link instead of pushing

--- src/FilmCard.js
+++ src/FilmCard.js
@@ -6,19 +6,17 @@ function FilmCard(props){
   const history = useHistory();
 
   function changeRoute() {
-    history.push(`/film-box/detail/movie/${props.id}`);
+    history.replace(`/film-box/detail/movie/${props.id}`);
     window.location.reload();
   }
 
   return (
-    <Link to={`/film-box/detail/movie/${props.id}`} className="link">
       <div onClick={changeRoute} className="film-card">
         <p className="film-card-title">{props.title}</p>
         <img className="film-card-poster" src={props.posterUrl}/>
         <p className="film-card-genre">{props.genres[0].name} {props.genres.length > 1 ? ` / ${props.genres[1].name}` : ""}</p>
         <p className="film-card-rating"><i class="fas fa-star"></i> {props.rating}</p>
       </div>
-    </Link>
 
   )
 }
