4b48d6abab9b6c1229cbc710758a6bcbdf524a00
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-03-04 16:32:00 -0600

    Updates

---
+++ precache-manifest.640bf86bb7f4e7df72e5113a7e3949fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e851db635432142ba599efca10ba0b",
+    "url": "/film-box/index.html"
+  },
+  {
+    "revision": "d9578711bc0ab03bdb03",
+    "url": "/film-box/static/css/2.c2d4b67f.chunk.css"
+  },
+  {
+    "revision": "2092ce44c5f8cab044b1",
+    "url": "/film-box/static/css/main.cac54ece.chunk.css"
+  },
+  {
+    "revision": "d9578711bc0ab03bdb03",
+    "url": "/film-box/static/js/2.1fdcbd3a.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/film-box/static/js/2.1fdcbd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2092ce44c5f8cab044b1",
+    "url": "/film-box/static/js/main.778a55de.chunk.js"
+  },
+  {
+    "revision": "ec5f1f0c8de49e53739f",
+    "url": "/film-box/static/js/runtime-main.757f5bab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/film-box/precache-manifest.474535862789badee3d5c0ac02df28df.js"
+  "/film-box/precache-manifest.640bf86bb7f4e7df72e5113a7e3949fd.js"
 );
 
 self.addEventListener('message', (event) => {
